c06c27fb83dabc6b660ca42a7862ec063f9e1b8d
rtm <raultorrecillamoreno@gmail.com>
2021-10-26 00:04:46 +0200

    v.5 - comprobarColumnas ACABADO

--- app/src/main/java/com/rtm/appsudoku/MainActivity.java
+++ app/src/main/java/com/rtm/appsudoku/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
     private EditText etn ;
     private String[] lista_et_id,lista_et_contenido;
+    private String[][][][] list = new String[3][3][3][3];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,23 +24,39 @@ public class MainActivity extends AppCompatActivity {
                 for (int f = 0; f < 3; f++){
                     for (int p = 0; p < 3; p++){
                         code = "et" + String.valueOf(r) + String.valueOf(c) + String.valueOf(f) + String.valueOf(p);
-                        int etID = getResources().getIdentifier(code,"id",getPackageName());
-                        etn = findViewById(etID);
-                        Log.i("ID",code);
+                      //  Log.i("ID",code);
                         lista_et_id[i]=code;
+                        list[r][c][f][p]=code;Log.i("lista"," "+ list[r][c][f][p]);
                         i++;
                     }
                 }
             }
         }
+
         imprimirListaEditText();
        // comprobarFilas();
-        comprobarColumnas();
+       // comprobarColumnas();
+        comprobarColumnasOK(0);
     }
     public void imprimirListaEditText(){
         for (int i=0; i<lista_et_id.length;i++){
             Log.d("idS",lista_et_id[i]);
         }
+    }
+    public void volcarContenidoOK(){ //Recoge todos los valores de los EditText
+        lista_et_contenido = new String[81];
+        for (int r = 0; r < 3 ; r++){
+            for (int c = 0; c < 3; c++){
+                for (int f = 0; f < 3; f++){
+                    for (int p = 0; p < 3; p++){
+                        int etID = getResources().getIdentifier(list[r][c][f][p], "id", getPackageName());
+                        etn = (EditText) findViewById(etID);
+                        list[r][c][f][p] = etn.getText().toString();Log.i("cont",""+list[r][c][f][p]);
+                    }
+                }
+            }
+        }
+
     }
     public void volcarContenido(){ //Recoge todos los valores de los EditText
         lista_et_contenido = new String[81];
@@ -48,6 +65,72 @@ public class MainActivity extends AppCompatActivity {
             etn = (EditText) findViewById(etID);
             lista_et_contenido[i] = etn.getText().toString();Log.i("cont",""+lista_et_contenido[i]);
         }
+    }
+    public void comprobarColumnasOK(int punto){
+        String[] nums ={"","","","","","","","",""};//9 huecosn por 9 posibles numeros
+        volcarContenidoOK();
+        int j=0,r=0,c=0,f=0,p=0/*punto*/;
+        //cuadrado 2
+        if (punto==3){//4 - columna
+            c=1;
+            f=0;
+            p=0;
+        }else if(punto==4){//5 - columna
+            c=1;
+            f=1;
+            p=1;
+        }else if (punto==5){//6 - columna
+            c=1;
+            f=2;
+            p=1;
+        }
+        //cuadrado 3
+        if (punto==7){//7 - columna
+            c=2;
+            f=0;
+            p=0;
+        }else if(punto==8){//8 - columna
+            c=2;
+            f=1;
+            p=1;
+        }else if (punto==9){//9 - columna
+            c=2;
+            f=2;
+            p=2;
+        }
+
+        while(r<3){
+                nums[j]=list[r][c][f][p];
+                Log.i("c "," "+nums[j]);
+                j++;
+                f++;
+            if (p==3){
+                p=0;
+            }
+            if (f==3){
+                f=0;
+                r++;
+            }
+
+        }
+        boolean repetido = false;
+        for (int w=0; w<nums.length;w++){
+            for (int q=0; q<nums.length;q++){
+                if (nums[w].equals(nums[q]) && w!=q) {
+                    repetido=true;
+                    break;
+                }
+            }
+            Log.i("numerous:","" + nums[w]);
+
+        }
+        String rep;
+        if (repetido==true){
+            rep="repetido";
+        }else{
+            rep="NO repetido";
+        }
+        Log.i("FILA COMPROBADA:","" + rep);
     }
     public void comprobarFilas(){ //Volcamos la fila que queremos comprobar en el array NUMS y posteriormente verificamos que no exista ninguna coincidencia
         String[] nums ={"","","","","","","","",""};//9 huecosn por 9 posibles numeros
