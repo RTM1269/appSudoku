fca7c30159a8104dec6d47f4d6a43674faab75a4
RTM1269 <raul.torrecilla.moreno@alumnojoyfe.iepgroup.es>
2021-10-26 18:20:44 +0200

    v.5 - filas y columnas ACABADO   vfinal

--- app/src/main/java/com/rtm/appsudoku/MainActivity.java
+++ app/src/main/java/com/rtm/appsudoku/MainActivity.java
@@ -9,48 +9,24 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
     private EditText etn ;
     private String[] lista_et_id,lista_et_contenido;
-    private String[][][][] list = new String[3][3][3][3];
+    private final String[][][][] list = new String[3][3][3][3];
+    private final String[][][][] list_idS = new String[3][3][3][3];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        etn = findViewById(R.id.et0000);
-
-        int i = 0;
-        lista_et_id = new String[81];
-        String code;
-        for (int r = 0; r < 3 ; r++){
-            for (int c = 0; c < 3; c++){
-                for (int f = 0; f < 3; f++){
-                    for (int p = 0; p < 3; p++){
-                        code = "et" + String.valueOf(r) + String.valueOf(c) + String.valueOf(f) + String.valueOf(p);
-                      //  Log.i("ID",code);
-                        lista_et_id[i]=code;
-                        list[r][c][f][p]=code;Log.i("lista"," "+ list[r][c][f][p]);
-                        i++;
-                    }
-                }
-            }
-        }
-
-        imprimirListaEditText();
-       // comprobarFilas();
-       // comprobarColumnas();
-       // comprobarColumnasOK(8);
-        comprobarFilasOK(4);
+        recogerIDs();
+        comprobarColumnasOKkkk();
+        comprobarFilasOK(5);
     }
-    public void imprimirListaEditText(){
-        for (int i=0; i<lista_et_id.length;i++){
-            Log.d("idS",lista_et_id[i]);
-        }
-    }
+
     public void volcarContenidoOK(){ //Recoge todos los valores de los EditText
         lista_et_contenido = new String[81];
         for (int r = 0; r < 3 ; r++){
             for (int c = 0; c < 3; c++){
                 for (int f = 0; f < 3; f++){
                     for (int p = 0; p < 3; p++){
-                        int etID = getResources().getIdentifier(list[r][c][f][p], "id", getPackageName());
+                        int etID = getResources().getIdentifier(list_idS[r][c][f][p], "id", getPackageName());
                         etn = (EditText) findViewById(etID);
                         list[r][c][f][p] = etn.getText().toString();Log.i("cont",""+list[r][c][f][p]);
                     }
@@ -58,14 +34,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-    }
-    public void volcarContenido(){ //Recoge todos los valores de los EditText
-        lista_et_contenido = new String[81];
-        for (int i = 0; i<lista_et_contenido.length;i++) {
-            int etID = getResources().getIdentifier(lista_et_id[i], "id", getPackageName());
-            etn = (EditText) findViewById(etID);
-            lista_et_contenido[i] = etn.getText().toString();Log.i("cont",""+lista_et_contenido[i]);
-        }
     }
     public void comprobarFilasOK(int punto){
         String[] nums ={"","","","","","","","",""};//9 huecosn por 9 posibles numeros
@@ -83,13 +51,13 @@ public class MainActivity extends AppCompatActivity {
             f=2;
         }
         //row 3
-        if (punto==7){//7 - fila
+        if (punto==6){//7 - fila
             r=2;
             f=0;
-        }else if(punto==8){//8 - fila
+        }else if(punto==7){//8 - fila
             r=2;
             f=1;
-        }else if (punto==9){//9 - fila
+        }else if (punto==8){//9 - fila
             r=2;
             f=2;
         }
@@ -142,13 +110,13 @@ public class MainActivity extends AppCompatActivity {
             p=1;
         }
         //cuadrado 3
-        if (punto==7){//7 - columna
+        if (punto==6){//7 - columna
             c=2;
             p=0;
-        }else if(punto==8){//8 - columna
+        }else if(punto==7){//8 - columna
             c=2;
             p=1;
-        }else if (punto==9){//9 - columna
+        }else if (punto==8){//9 - columna
             c=2;
             p=2;
         }
@@ -184,58 +152,85 @@ public class MainActivity extends AppCompatActivity {
         }else{
             rep="NO repetido";
         }
-        Log.i("FILA COMPROBADA:","" + rep);
+        Log.i("Columna COMPROBADA:","" + rep);
     }
-    public void comprobarFilas(){ //Volcamos la fila que queremos comprobar en el array NUMS y posteriormente verificamos que no exista ninguna coincidencia
+    public void comprobarColumnasOKkkk(){
+        int punto=0;
         String[] nums ={"","","","","","","","",""};//9 huecosn por 9 posibles numeros
-        volcarContenido();
-        int j=0,k=0,cont=0;
-        while(j<9){
-            if (cont<3){
-                nums[j]=lista_et_contenido[k];
+        volcarContenidoOK();
+        while (punto<9){
+            int j=0,r=0,c=0,f=0,p=punto/*punto*/;
+            //cuadrado 2
+            if (punto==3){//4 - columna
+                c=1;
+                        p=0;
+            }else if(punto==4){//5 - columna
+                c=1;
+                p=1;
+            }else if (punto==5){//6 - columna
+                c=1;
+                p=1;
+            }
+            //cuadrado 3
+            if (punto==6){//7 - columna
+                c=2;
+                p=0;
+            }else if(punto==7){//8 - columna
+                c=2;
+                p=1;
+            }else if (punto==8){//9 - columna
+                c=2;
+                p=2;
+            }
+
+            while(r<3){
+                nums[j]=list[r][c][f][p];
+                Log.i("c "," "+nums[j]);
                 j++;
-                k++;
-                cont++;
-            }else {
-                k=k+6;
-                cont=0;
-            }
-
-        }
-        boolean repetido = false;
-        for (int w=0; w<nums.length;w++){
-            for (int q=0; q<nums.length;q++){
-                if (nums[w].equals(nums[q]) && w!=q) {
-                    repetido=true;
-                    break;
+                f++;
+                if (p==3){
+                    p=0;
                 }
-            }
-            Log.i("numerous:","" + nums[w]);
+                if (f==3){
+                    f=0;
+                    r++;
+                }
 
-        }
-        String rep;
-        if (repetido==true){
-            rep="repetido";
-        }else{
-            rep="NO repetido";
-        }
-        Log.i("FILA COMPROBADA:","" + rep);
-    }
-    public void comprobarColumnas(){
-        String[] nums ={"","","","","","","","",""};//9 huecos por 9 posibles numeros
-        volcarContenido();
-        int j=0,cont=0,i=0;
-        while(i<9){
-            if(cont < 3){
-                nums[i]=lista_et_contenido[j];
-               // Log.i("c",""+nums[i]);
-                Log.i("li"," "+ lista_et_contenido[j]);
-                i++;
-                j=j+3;
-                cont++;
+            }
+            boolean repetido = false;
+            for (int w=0; w<nums.length;w++){
+                for (int q=0; q<nums.length;q++){
+                    if (nums[w].equals(nums[q]) && w!=q) {
+                        repetido=true;
+                        break;
+                    }
+                }
+                Log.i("numerous:","" + nums[w]);
+
+            }
+            String rep;
+            if (repetido==true){
+                rep="repetido";
             }else{
-                j=j+18;
-                cont=0;
+                rep="NO repetido";
+            }
+            Log.i("Columna "+punto+" COMPROBADA","" + rep);
+            punto++;
+        }
+
+    }
+    public void recogerIDs(){
+        String code;
+        for (int r = 0; r < 3 ; r++){
+            for (int c = 0; c < 3; c++){
+                for (int f = 0; f < 3; f++){
+                    for (int p = 0; p < 3; p++){
+                        code = "et" + String.valueOf(r) + String.valueOf(c) + String.valueOf(f) + String.valueOf(p);
+                        //  Log.i("ID",code);
+                        list_idS[r][c][f][p]=code;
+                        // list[r][c][f][p]=code;Log.i("lista"," "+ list[r][c][f][p]);
+                    }
+                }
             }
         }
     }
